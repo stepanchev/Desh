f5da24eb4d80f8981b89167221c9e6856184ae54
Bless <artemstepantev@gmail.com>
2026-09-26 00:10:15 +0400

    feat: add modal for adding tasks

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const openPanel = document.getElementById('headerButtonOpenPanal')
-const sidebar = document.getElementById("sidebar")
-openPanel.addEventListener('click', () => {
-    sidebar.classList.toggle('open')
-})
+const addTask = document.getElementById("addTaskButton");
+const closeModalTaskButton = document.getElementById("closeModalAddTask");
+const modalOverlay = document.getElementById("modalOverlay");
+const cancelButtonModal = document.getElementById("cancelButtonModal");
+
+function openModalAddTask() {
+  modalOverlay.classList.toggle("active");
+}
+
+function closeModalAddTask() {
+  modalOverlay.classList.remove("active");
+}
+
+addTask.addEventListener("click", () => {
+  openModalAddTask();
+});
+
+closeModalTaskButton.addEventListener("click", () => {
+  closeModalAddTask();
+});
+
+cancelButtonModal.addEventListener("click", () => {
+  closeModalAddTask();
+});
